7e39fe5dc42ea6b66cfbb1a63692092ead643022
Synaxis <oiacow@gmail.com>
2018-03-01 19:43:49 -0300

    var, let, const

---
+++ app.js
@@ -0,0 +1,55 @@
+// // var, let, const 
+
+// var name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+
+// Init var
+// var greeting;
+// console.log(greeting);
+// greeting = 'Hello';
+// console.log(greeting);
+
+
+// var CAN include
+// //letters , numbers, _, $
+// //Can not start with number
+// //var 1name ='John';
+
+
+// Multi word vars
+// var firstname = 'John'; // Camel Case
+// var first_name = 'Sara'; //Underscore
+// var FirstName = 'Tom'; // Pascal case
+// var firstname;
+
+
+// Let 
+// let name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+
+// // Const
+// const name = 'John';
+// console.log(name);
+// // Can not reassing
+// name = 'Sara';
+
+
+//Reassing value to const
+const person = {
+    name: 'John',
+    age: 30
+}
+person.name = 'Sara';
+person.age = 32;
+console.log(person);
+
+const numbers = [1,2,3,4,5];
+numbers.push(6);
+
+conesole.log(numbers);
