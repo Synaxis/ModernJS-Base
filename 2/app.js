0ccf19521e57a1776e4feb0246b0eda3ab73f6fd
Synaxis <oiacow@gmail.com>
2018-03-01 19:57:45 -0300

    Level 2 done

---
+++ 2/app.js
@@ -0,0 +1,32 @@
+// Primitive 
+
+// String
+const name = 'John Doe';
+
+// Number
+const age = '45';
+//const age = 45;  45 = string 
+
+// Boolen
+const hasKids = true;
+
+// Null
+const car = null;
+
+// Undefine
+let test;
+// Symbol
+const sym = Symbol();
+
+// Reference types - object
+// array
+const hobbies = ['movies', 'music'];
+// Object Literal 
+const address = {
+    city: 'Boston',
+    state: 'MA'
+}
+const today = new Date();
+console.log(today);
+
+console.log(typeof today);
